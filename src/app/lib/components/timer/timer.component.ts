00236981d90aeef09d3015e19c85cd4e2357ceb0
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-17 19:36:18 +0200

    Write the code to handle mode switch

--- src/app/lib/components/timer/timer.component.ts
+++ src/app/lib/components/timer/timer.component.ts
@@ -7,4 +7,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     templateUrl: './timer.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TimerComponent {}
+export class TimerComponent {
+    // Constants
+    public readonly focusMode: number = 0;
+    public readonly shortBreakMode: number = 1;
+    public readonly longBreakMode: number = 2;
+
+    private _mode: number = this.focusMode;
+
+    handleModeChange(mode: number): void {
+        console.log('Theme changed to : ', mode);
+        this._mode = mode;
+    }
+
+    get mode(): number {
+        return this._mode;
+    }
+}
