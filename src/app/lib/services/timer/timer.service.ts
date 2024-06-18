a7f781ccc51cc0e23518a77286d60a1adb4eff09
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-18 18:08:49 +0200

    Now update the counter and automatically switch from short to focus

--- src/app/lib/services/timer/timer.service.ts
+++ src/app/lib/services/timer/timer.service.ts
@@ -12,16 +12,20 @@ export class TimerService {
     public readonly longBreakMode: string = 'long-break';
 
     // public readonly focusDuration: number = 60 * 25;
-    public readonly focusDuration: number = 5;
-    public readonly shortBreakDuration: number = 60 * 5;
-    public readonly longBreakDuration: number = 60 * 15;
+    // public readonly shortBreakDuration: number = 60 * 5;
+    // public readonly longBreakDuration: number = 60 * 15;
+    public readonly focusDuration: number = 2;
+    public readonly shortBreakDuration: number = 3;
+    public readonly longBreakDuration: number = 4;
 
     private _baseTime = new BehaviorSubject<number>(this.focusDuration);
     private _time = new BehaviorSubject<number>(this._baseTime.getValue());
     private _mode = new BehaviorSubject<string>(this.focusMode);
+    private _cycles = new BehaviorSubject<number>(1);
     baseTime$: Observable<number> = this._baseTime.asObservable();
     time$: Observable<number> = this._time.asObservable();
     mode$: Observable<string> = this._mode.asObservable();
+    cycles$: Observable<number> = this._cycles.asObservable();
 
     private _timer: ReturnType<typeof setInterval> | null = null;
 
@@ -52,6 +56,18 @@ export class TimerService {
         return this._mode.getValue();
     }
 
+    setCycles(c: number): void {
+        this._cycles.next(c);
+    }
+
+    getCycles(): number {
+        return this._cycles.getValue();
+    }
+
+    incrementCycles(): void {
+        this._cycles.next(this._cycles.getValue() + 1);
+    }
+
     toggleTimer(): void {
         if (!this._timer) {
             this._timer = setInterval(() => {
@@ -60,11 +76,18 @@ export class TimerService {
                     this._time.next(this._time.getValue() - 1);
                 } else {
                     // Clear timer and change mode when reaching 0
-                    this.setMode(this.shortBreakMode);
-                    this.setTime(this.shortBreakDuration);
-                    this.setBaseTime(this.shortBreakDuration);
                     clearInterval(this._timer as ReturnType<typeof setInterval>);
                     this._timer = null;
+                    if (this.getMode() == this.focusMode) {
+                        this.setMode(this.shortBreakMode);
+                        this.setTime(this.shortBreakDuration);
+                        this.setBaseTime(this.shortBreakDuration);
+                    } else if (this.getMode() == this.shortBreakMode) {
+                        this.setMode(this.focusMode);
+                        this.setTime(this.focusDuration);
+                        this.setBaseTime(this.focusDuration);
+                        this.incrementCycles();
+                    }
                 }
             }, 1000);
         } else {
