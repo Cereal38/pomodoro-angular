be07cb056128841cc98cc0c93a7a6ef8fe8e3343
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-18 17:41:23 +0200

    The mode is now handled directly in the timer handler

--- src/app/lib/components/timer/timer.component.ts
+++ src/app/lib/components/timer/timer.component.ts
@@ -10,47 +10,27 @@ import { TimerService } from '@lib/services/timer/timer.service';
     templateUrl: './timer.component.html',
 })
 export class TimerComponent implements OnInit {
-    // Constants
-    public readonly focusMode: number = 0;
-    public readonly shortBreakMode: number = 1;
-    public readonly longBreakMode: number = 2;
-
     currentTheme!: AppTheme | null;
 
-    mode!: number;
+    mode!: string;
     time!: number; // In seconds
 
     constructor(public timerService: TimerService, public themeService: ThemeService) {}
 
     // Init mode on mount
     ngOnInit(): void {
-        this.mode = this.focusMode;
-
         // Subscribe to the timer service
         this.timerService.time$.subscribe((t) => {
             this.time = t;
         });
-    }
 
-    handleModeChange(mode: number): void {
-        console.log('Mode changed to : ', mode);
-        switch (mode) {
-            case this.focusMode:
-                this.themeService.setTheme('focus');
-                break;
-            case this.shortBreakMode:
-                this.themeService.setTheme('short-break');
-                break;
-            case this.longBreakMode:
-                this.themeService.setTheme('long-break');
-                break;
-        }
-
-        this.mode = mode;
+        this.timerService.mode$.subscribe((m) => {
+            this.mode = m;
+        });
     }
 
     get formattedTime(): string {
-        return `${Math.floor(this.time / 60)}:${String(this.time % 60).padStart(2, '0')}`;
+        return `${String(Math.floor(this.time / 60)).padStart(2, '0')}:${String(this.time % 60).padStart(2, '0')}`;
     }
 
     // Pause and play timer and space press
--- src/app/lib/services/timer/timer.service.ts
+++ src/app/lib/services/timer/timer.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { AppTheme, ThemeService } from '../theme';
 
 @Injectable({
     providedIn: 'root',
 })
 export class TimerService {
+    // Constants
+    public readonly focusMode: string = 'focus';
+    public readonly shortBreakMode: string = 'short-break';
+    public readonly longBreakMode: string = 'long-break';
+
     private _baseTime = new BehaviorSubject<number>(120);
     private _time = new BehaviorSubject<number>(this._baseTime.getValue());
+    private _mode = new BehaviorSubject<string>(this.focusMode);
     baseTime$: Observable<number> = this._baseTime.asObservable();
     time$: Observable<number> = this._time.asObservable();
+    mode$: Observable<string> = this._mode.asObservable();
 
     private _timer: ReturnType<typeof setInterval> | null = null;
+
+    constructor(private _themeService: ThemeService) {}
 
     setTime(t: number): void {
         this._time.next(t);
@@ -26,6 +36,15 @@ export class TimerService {
 
     getBaseTime(): number {
         return this._baseTime.getValue();
+    }
+
+    setMode(m: string): void {
+        this._mode.next(m);
+        this._themeService.setTheme(m as AppTheme);
+    }
+
+    getMode(): string {
+        return this._mode.getValue();
     }
 
     toggleTimer(): void {
